7ff895516c7c4916501c705c054ad05d7c547169
harshilghinaiya <ghinaiyaharshil1@gmail.com>
2025-04-17 16:23:02 +0530

    Add files via upload

---
+++ minnumber.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int a,b;
+	
+	printf("enter value of a");
+	scanf("%d",a);
+	
+	printf("enter value of b");
+	scanf("d",&b);
+	
+	if(a>b)
+	{
+		printf("a is min number");
+	}
+	else
+	{
+		printf("b is min number");
+	}
+}
+
+
+
