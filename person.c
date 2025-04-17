f8c982d1f1811c4f25a1d408907b37153ab90d01
harshilghinaiya <ghinaiyaharshil1@gmail.com>
2025-04-17 16:47:39 +0530

    Add files via upload

---
+++ person.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int person,b=16
+	
+	printf("enter value of a");
+	scanf("%d",&person);
+	
+	if(person>b)
+	{
+		printf("that person is elejeble for voting");
+	}
+	else
+	{
+		a>b
+		printf("that person is not elejeble for voting");
+	}
+}
