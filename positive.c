352b4fb0748eff833f80d175a0af38f25e6da78d
harshilghinaiya <ghinaiyaharshil1@gmail.com>
2025-04-17 16:41:48 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,19 @@
+#inclde<stdio.h>
+
+int main()
+
+{
+	 int a,b;
+	 
+	 printf("enter value of a");
+	 scanf("%d",&a);
+	 
+	 if(a>b)
+	 {
+	 	printf("a is positive");
+	 }
+	 else
+	 {
+	 	printf("a is nagetive");
+	 }
+}
