99fb3e998e77f4d7bbfff0049c1b2041d393a477
harshilghinaiya <ghinaiyaharshil1@gmail.com>
2025-04-24 15:41:33 +0530

    Add files via upload

---
+++ voting.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	 int person,b=17;
+	 
+    printf("enter your age:");
+    scanf("%d",&person);
+    
+    if(person>b){
+    	printf("that person is elejeble for votig.");
+	}
+	else
+	{
+		printf("that person is not elejeble for votig.");
+	}
+}
