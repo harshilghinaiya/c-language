4acc49b4bcdc962bbf9f9d2059a92a6b538464d1
harshilghinaiya <ghinaiyaharshil1@gmail.com>
2025-05-16 16:47:39 +0530

    Update a.c

--- a.c
+++ a.c
@@ -2,33 +2,31 @@
 
 int main() {
     int n, i;
-    int arr[100];         // Static array with a max size
-    int *p = arr;         // Pointer to array
-    int **pptr = &p;      // Chain of pointer (pointer to pointer)
+    int arr[100];        
+    int *p = arr;         
+    int **pp = &p;      
+    
 
-    // Input array size
     printf("Enter the array's size: ");
     scanf("%d", &n);
 
-    // Input array elements
+  
     printf("Enter array elements:\n");
     for(i = 0; i < n; i++) {
         printf("a[%d] = ", i);
-        scanf("%d", &(*pptr)[i]);
+        scanf("%d", &(*pp)[i]);
     }
 
-    // Square each element
+  
     for(i = 0; i < n; i++) {
-        (*pptr)[i] = (*pptr)[i] * (*pptr)[i];
+        (*pp)[i] = (*pp)[i] * (*pp)[i];
     }
 
-    // Print in reverse order
+  
     printf("Reversed array elements:\n");
     for(i = n - 1; i >= 0; i--) {
-        printf("%d", (*pptr)[i]);
+        printf("%d", (*pp)[i]);
         if(i != 0)
             printf(", ");
     }
-
-    return 0;
 }
